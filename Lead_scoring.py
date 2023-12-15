d1cdaa06ddcbe659accf05cdd8dbe7030efe4f41
satyam singh bhadouriya <139864397+Nutron00@users.noreply.github.com>
2023-12-15 10:53:26 +0530

    Delete Lead_scoring.py

--- Lead_scoring.py
+++
@@ -1,9 +0,0 @@
-
-# first importing the basic libraries
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import warnings
-warnings.filterwarnings('ignore')
-
